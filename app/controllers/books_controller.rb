af96cf2087672fa81a9034257740cb319ffa1520
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2020-12-24 15:28:45 +0000

    [Update]control

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,7 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def show
@@ -30,6 +31,9 @@ class BooksController < ApplicationController
   end
   
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to 
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  root to: "homes#top"
   devise_for :users
   resources :users
   resources :books, only: [:new, :create, :edit, :update, :index, :show, :destroy]
